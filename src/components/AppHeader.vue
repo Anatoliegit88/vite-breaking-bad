8a6c3c27bfa25b7abeb7fd29e74a9d2afeed40eb
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-11-10 19:28:57 +0100

    first step

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header>
+    <!-- <img
+      class="img"
+      src="./assets/breaking-bad-2-180x180-1343174296.png"
+      alt="" /> -->
+    <h1>Breaking Bad Api</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.img {
+  width: 40px;
+}
+</style>
